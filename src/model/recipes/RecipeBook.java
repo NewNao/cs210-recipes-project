1fab505fdc6754f68d692059f9115e137f9e25e3
Naoreen <o7r1b@cs.ubc.ca>
2018-12-27 11:07:38 -0700

    Created RecipeBook class for storing recipes, with constructor, add, and remove methods.

---
+++ src/model/recipes/RecipeBook.java
@@ -0,0 +1,24 @@
+package model.recipes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RecipeBook {
+    private Set<Recipe> recipes;
+
+    public RecipeBook(){
+        recipes = new HashSet<>();
+    }
+
+    public void addRecipe(Recipe r){
+        if(!recipes.contains(r)) {
+            recipes.add(r);
+        }
+    }
+
+    public void removeRecipe(Recipe r){
+        if(recipes.contains(r)){
+            recipes.remove(r);
+        }
+    }
+}
